94aa640009d95482933219d101ed54003046a7ac
ryusyo621 <76475977+ryusyo621@users.noreply.github.com>
2021-01-21 12:06:35 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validatas :nickname, presence: true
+  validatas :first_name, presence: true
+  validatas :last_name, presence: true
+  validatas :huri_first_name, presence: true
+  validatas :huri_last_name, presence: true
+  validatas :birthday, presence: true
+
 end
