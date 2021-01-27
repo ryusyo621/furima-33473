15aade17ee0fdb3d4a8fe3cd5afa208038e25b93
ryusyo621 <76475977+ryusyo621@users.noreply.github.com>
2021-01-27 11:53:55 +0900

    アソシエーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates :product_name
+    validates :product_text
+    validates :category_id
+    validates :state_id
+    validates :burden_id
+    validates :area_id
+    validates :shiping_id
+    validates :price
+  end
 end
