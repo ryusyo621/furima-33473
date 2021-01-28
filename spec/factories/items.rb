fbb1e70f96bc1d95d26c01ee2676acf4a644b93f
ryusyo621 <76475977+ryusyo621@users.noreply.github.com>
2021-01-28 11:50:25 +0900

    単体テストコード雛形

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    product_name {"test"}
+    product_text {"test@test.com"}
+    category_id {"00000f"}
+    state_id {password}
+    burden_id {"山田"}
+    area_id {"太郎"}
+    shiping_id {"ヤマダ"}
+    price {"タロウ"}
+    association :user
   end
 end
