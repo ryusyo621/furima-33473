a21a840508dc0a4e0a4512c5a7fe5c2c61e4af97
ryusyo621 <76475977+ryusyo621@users.noreply.github.com>
2021-01-29 10:49:31 +0900

    テストコード完成2

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -6,7 +6,16 @@ RSpec.describe Item, type: :model do
   end
   describe '出品新規登録' do
     context '出品登録できるとき' do
+      it '販売価格は半角数字で保存可能であること' do
+        @item.price = '10000'
+        expect(@item).to be_valid
+      end
+      it '価格の範囲が、¥300~¥9,999,999の間であること' do
+        @item.price = '10000'
+        expect(@item).to be_valid
+      end
     end
+
     context '出品登録できないとき' do
       it '商品画像を1枚つけることが必須であること' do
         @item.image = nil
@@ -53,10 +62,6 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price can't be blank")
       end
-      it '価格の範囲が、¥300~¥9,999,999の間であること' do
-        @item.price = '10000'
-        expect(@item).to be_valid
-      end
       it '価格の範囲が、¥300未満だと保存できないこと' do
         @item.price = '299'
         @item.valid?
@@ -66,11 +71,6 @@ RSpec.describe Item, type: :model do
         @item.price = '10000000'
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not included in the list')
-      end
-
-      it '販売価格は半角数字で保存可能であること' do
-        @item.price = '10000'
-        expect(@item).to be_valid
       end
       it '販売価格は全角数字があると保存できないこと' do
         @item.price = '０００００'
@@ -82,7 +82,6 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not included in the list')
       end
-
       it '販売価格は半角英字があると保存できないこと' do
         @item.price = 'fffff'
         @item.valid?
