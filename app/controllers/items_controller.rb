e4b9eb4a241c6af962976f3245280148ed9950ac
ryusyo621 <76475977+ryusyo621@users.noreply.github.com>
2021-01-31 16:00:20 +0900

    削除ボタン作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_item, only: [:show, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :item_move, only: [:edit, :update]
 
   def index
@@ -35,8 +35,12 @@ class ItemsController < ApplicationController
     end
   end
 
-  # def destroy
-  # end
+  def destroy
+    if @item.destroy
+      redirect_to root_path
+    end
+    
+  end
 
   private
 
