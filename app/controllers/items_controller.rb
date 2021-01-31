bebb6be3f5e51f49a2d041e83689fdc733b4430e
ryusyo621 <76475977+ryusyo621@users.noreply.github.com>
2021-01-31 16:37:48 +0900

    商品削除機能修正②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :item_move, only: [:edit, :update]
+  before_action :item_move, only: [:edit, :update, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -35,7 +35,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    redirect_to root_path if @item.destroy
+    if @item.destroy
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
