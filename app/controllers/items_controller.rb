d5c336795866b6575a43cbe02d85cd612f5824d4
ryusyo621 <76475977+ryusyo621@users.noreply.github.com>
2021-01-31 15:01:10 +0900

    情報編集機能修正①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
+  before_action :item_movem only: [:edit, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -19,16 +21,13 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
-    redirect_to action: :index unless current_user.id == @item.user_id
   end
 
+
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -41,8 +40,17 @@ class ItemsController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
-    params.require(:item).permit(:product_name, :product_text, :category_id, :state_id, :burden_id, :area_id, :shiping_id,
-                                 :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:product_name, :product_text, :category_id, :state_id, :burden_id, :area_id, :shiping_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def item_move
+    unless current_user.id == @item.user_id
+      redirect_to items_path,method: :get
+    end
   end
 end
