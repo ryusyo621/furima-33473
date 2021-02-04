5a838f5c59ac1024fd94b444815535e317261fa7
ryusyo621 <76475977+ryusyo621@users.noreply.github.com>
2021-02-04 11:47:58 +0900

    ファクトリーボット作成

---
+++ spec/factories/orders.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+  factory :order do
+    post_number { '123-4567' }
+    area_id { '2' }
+    city { '横浜市緑区' }
+    address { '青山1-1-1' }
+    building { '柳ビル103' }
+    phone_number { '09012345678' }
+  end
+end
